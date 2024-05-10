5937acd4599e04303df25bac2961db59fbe923d8
Stan Yu <stanyu123@gmail.com>
2024-05-10 12:17:41 -0700

    Debugging player getting stuck after releasing dash

--- Entities/Player.js
+++ Entities/Player.js
@@ -1,56 +1,55 @@
-class Player extends Character{
-    constructor(game) {
-        super("char", "Reimu", game, 
-            400, 400, 
-            378, 
-            400, 700 - 378, 
-            100);
-        this.facing = 0;
-        this.setController(new PlayerController(this, game));
-    };
+class Player extends Character {
+  constructor(game) {
+    super("char", "Reimu", game, 400, 400, 378, 400, 700 - 378, 100);
+    this.facing = 0;
+    this.setController(new PlayerController(this, game));
+  }
 
-    loadAnimations() {
-        this.makeAnimation(0, 0, 0, 1, 1); //IDLE
-        this.makeAnimation(1, 1, 0, 1, 1); //CROUCH
-        this.makeAnimation(2, 2, 0, 1, 1); //WALK F
-        this.makeAnimation(3, 3, 0, 1, 1); //WALK B
-        this.makeAnimation(4, 4, 0, 1, 1); //DASH
-        this.makeAnimation(5, 5, 0, 1, 1); //BACKDASH
-        
-        this.makeAnimation(6, 6, 0, 1, 1); //PREJUMP
-        this.makeAnimation(7, 6, 1, 1, 1); //JUMP
-        this.makeAnimation(8, 6, 2, 1, 1); //FALL
-    };
+  loadAnimations() {
+    this.makeAnimation(0, 0, 0, 1, 1); //IDLE
+    this.makeAnimation(1, 1, 0, 1, 1); //CROUCH
+    this.makeAnimation(2, 2, 0, 1, 1); //WALK F
+    this.makeAnimation(3, 3, 0, 1, 1); //WALK B
+    this.makeAnimation(4, 4, 0, 1, 1); //DASH
+    this.makeAnimation(5, 5, 0, 1, 1); //BACKDASH
 
-    updateBB() {
-        switch(this.state) {
-            default:
-                if (this.facing == 0) {
-                    
-                } else {
-                    
-                }
-            break;
-        } 
-    };
+    this.makeAnimation(6, 6, 0, 1, 1); //PREJUMP
+    this.makeAnimation(7, 6, 1, 1, 1); //JUMP
+    this.makeAnimation(8, 6, 2, 1, 1); //FALL
+    this.makeAnimation(9, 4, 0, 1, 1); //DASH DECELARATION
+  }
 
-
-    update() {
-        this.controller.update();
-        this.updateBB();
-    };
-
-    draw(ctx) {
-        // this.drawShadow(ctx);
-        this.animations[this.facing][this.state].drawFrame(this.game.clockTick, ctx, this.x, this.y);
-
-        if (this.game.boxView) {
-            ctx.strokeStyle = "yellow";
-            ctx.beginPath();
-            
-            ctx.rect(this.BB.x, this.BB.y, this.BB.width, this.BB.height)
-            
-            ctx.stroke();
+  updateBB() {
+    switch (this.state) {
+      default:
+        if (this.facing == 0) {
+        } else {
         }
-    };
+        break;
+    }
+  }
+
+  update() {
+    this.controller.update();
+    this.updateBB();
+  }
+
+  draw(ctx) {
+    // this.drawShadow(ctx);
+    this.animations[this.facing][this.state].drawFrame(
+      this.game.clockTick,
+      ctx,
+      this.x,
+      this.y
+    );
+
+    if (this.game.boxView) {
+      ctx.strokeStyle = "yellow";
+      ctx.beginPath();
+
+      ctx.rect(this.BB.x, this.BB.y, this.BB.width, this.BB.height);
+
+      ctx.stroke();
+    }
+  }
 }
--- components/PlayerController.js
+++ components/PlayerController.js
@@ -1,118 +1,145 @@
 class PlayerController {
-    constructor(player, game) {
-        this.player = player;
-        this.game = game;
+  constructor(player, game) {
+    this.player = player;
+    this.game = game;
 
-        this.yVelocity = 0;
-        this.xVelocity = 0;
+    this.yVelocity = 0;
+    this.xVelocity = 0;
 
-        this.grounded = false;
+    this.grounded = false;
+    this.prejump = 0;
+    this.dashing = false;
+
+    this.prejumpFrames = 5;
+    this.fWalk = 150;
+    this.bWalk = 150;
+    this.dashSpeed = 8;
+    this.dashAccel = 1;
+    this.dashTimer = 0;
+    this.dashDuration = 40;
+    this.backdashTimer = 30;
+    this.backdashDuration = 40;
+    this.backdashVelocity = 8;
+
+    this.jumpStrength = 11;
+    this.gravity = 0.2;
+    this.jumpDistance = 5;
+  }
+
+  updateState() {
+    if (this.player.state == 6) {
+      // ANY LOCK OUT STATES HERE
+      this.prejump -= 1;
+      if (this.prejump <= 0) {
         this.prejump = 0;
-        this.dashing = false;
+        this.player.state = 7;
+      }
+    } else if (this.dashing) {
+      if (inputManager.latest.movement == 6 || inputManager.dash) {
+        this.player.state = 4;
+      } else {
+        this.player.state = 9;
+        this.dashTimer = this.dashDuration;
+      }
+    } else if (this.player.state == 9 && this.dashTimer > 0) {
+      this.dashTimer -= 1;
+    } else if (this.player.state == 5 && this.backdashTimer > 0) {
+      this.backdashTimer -= 1;
+    } else if (this.grounded) {
+      //GROUNDED STATES
+      this.player.state = 0;
 
-        this.prejumpFrames = 5;
-        this.fWalk = 150;
-        this.bWalk = 150;
-        this.dashSpeed = 8;
-        this.dashAccel = 1;
-        this.backdashTimer = 30;
-        this.backdashDuration = 40;
-        this.backdashVelocity = 8;
-
-        this.jumpStrength = 11;
-        this.gravity = 0.20;
-        this.jumpDistance = 5;
-    };
-
-    updateState() {
-        if (this.player.state == 6) { // ANY LOCK OUT STATES HERE
-            this.prejump -= 1;
-            if (this.prejump <= 0) {
-                this.prejump = 0;
-                this.player.state = 7;
-            }
-        } else if (this.dashing) {
-            if (inputManager.latest.movement == 6 || inputManager.dash) {
-                this.player.state = 4;
-            } else this.dashing = false;
-        } else if (this.player.state == 5 && this.backdashTimer > 0) {
-            this.backdashTimer -= 1;
+      if (inputManager.down) {
+        this.player.state = 1;
+      } else {
+        if (inputManager.right && !inputManager.left) {
+          if ((this.player.facing = 0)) {
+            this.player.state = 3;
+          } else {
+            this.player.state = 2;
+          }
+          //   this.dashing = inputManager.dashCheck(6) || inputManager.dash; //forward dash
+          this.dashing = inputManager.dashCheck(6) || inputManager.dash;
         }
-        else if (this.grounded) { //GROUNDED STATES
-            this.player.state = 0;
+        if (inputManager.left && !inputManager.right) {
+          if ((this.player.facing = 0)) this.player.state = 2;
+          else this.player.state = 3;
 
-            if (inputManager.down) {
-                this.player.state = 1;
-            } else {
-                if (inputManager.right && !inputManager.left) {
-                    if (this.player.facing = 0) {
-                        this.player.state = 3;
-                    } else {
-                        this.player.state = 2;
-                    }
-                    this.dashing = inputManager.dashCheck(6) || inputManager.dash; //forward dash
-                }
-                if (inputManager.left && !inputManager.right) {
-                    if (this.player.facing = 0) this.player.state = 2;
-                    else this.player.state = 3;
-
-                    if (inputManager.dashCheck(4) || inputManager.dash) {
-                        //back dash
-                        this.player.state = 5;
-                        this.backdashTimer = this.backdashDuration;
-                    } 
-                }
-                if ((inputManager.up && !inputManager.upHold) && this.prejump == 0) { //START THE JUMP
-                    this.player.state = 6;
-                    this.prejump = this.prejumpFrames;
-                }
-            }
-        } else { //AIRBORNE STATES
-            if (this.yVelocity >= 0) {
-                this.player.state = 8;
-            }
+          if (inputManager.dashCheck(4) || inputManager.dash) {
+            //back dash
+            this.player.state = 5;
+            this.backdashTimer = this.backdashDuration;
+          }
         }
-
-
-        
-    };
-
-    updateMovement() {
-        this.player.y += this.yVelocity;
-        this.player.x += this.xVelocity;
-        if (this.player.state == 2) { this.player.x += this.fWalk * gameEngine.clockTick;}
-        if (this.player.state == 3) { this.player.x -= this.fWalk * gameEngine.clockTick;}
-        if (this.player.state == 4) {
-            if (this.xVelocity < this.dashSpeed) {
-                this.xVelocity += this.dashAccel;
-                if (this.xVelocity > this.dashSpeed) this.xVelocity = this.dashSpeed; 
-            }
+        if (inputManager.up && !inputManager.upHold && this.prejump == 0) {
+          //START THE JUMP
+          this.player.state = 6;
+          this.prejump = this.prejumpFrames;
         }
-        if (this.player.state == 5) {
-            this.xVelocity = -this.backdashVelocity;
-        }
-        if (this.player.state == 7 && this.grounded) { // JUMP
-            this.grounded = false;
-            this.yVelocity = -this.jumpStrength;
-            if (inputManager.right) {
-                this.xVelocity = this.jumpDistance;
-            }
-            if (inputManager.left) {
-                this.xVelocity = -this.jumpDistance;
-            }
-        }
+      }
+    } else {
+      //AIRBORNE STATES
+      if (this.yVelocity >= 0) {
+        this.player.state = 8;
+      }
+    }
+  }
 
-        if (!this.grounded) this.yVelocity += this.gravity; //Gravity
-        if (this.player.y + this.player.yBoxOffset >= floor && this.yVelocity >= 0) { //Check grounded
-            this.yVelocity = 0;
-            if (this.player.state != 4 && this.player.state != 5) this.xVelocity = 0;
-            this.player.y = floor - this.player.yBoxOffset;
-            this.grounded = true;
-        }
-    };
+  updateMovement() {
+    this.player.y += this.yVelocity;
+    this.player.x += this.xVelocity;
+    if (this.player.state == 2) {
+      this.player.x += this.fWalk * gameEngine.clockTick;
+    }
+    if (this.player.state == 3) {
+      this.player.x -= this.fWalk * gameEngine.clockTick;
+    }
+    // Handle dash state
+    if (this.player.state == 4) {
+      // If dashing, accelerate up to dash speed
+      if (this.xVelocity < this.dashSpeed) {
+        this.xVelocity += this.dashAccel;
+        if (this.xVelocity > this.dashSpeed) this.xVelocity = this.dashSpeed;
+      }
+    }
 
-    update() {
-        this.updateState();
-        this.updateMovement();
-    };
+    if (this.player.state == 9) {
+      if (this.xVelocity > 0) {
+        this.xVelocity -= this.dashAccel;
+        if (this.xVelocity < 0) this.xVelocity = 0; // Ensure velocity doesn't go negative
+      }
+    }
+
+    if (this.player.state == 5) {
+      this.xVelocity = -this.backdashVelocity;
+    }
+    if (this.player.state == 7 && this.grounded) {
+      // JUMP
+      this.grounded = false;
+      this.yVelocity = -this.jumpStrength;
+      if (inputManager.right) {
+        this.xVelocity = this.jumpDistance;
+      }
+      if (inputManager.left) {
+        this.xVelocity = -this.jumpDistance;
+      }
+    }
+
+    if (!this.grounded) this.yVelocity += this.gravity; //Gravity
+    if (
+      this.player.y + this.player.yBoxOffset >= floor &&
+      this.yVelocity >= 0
+    ) {
+      //Check grounded
+      this.yVelocity = 0;
+      if (this.player.state != 4 && this.player.state != 5) this.xVelocity = 0;
+      this.player.y = floor - this.player.yBoxOffset;
+      this.grounded = true;
+    }
+  }
+
+  update() {
+    this.updateState();
+    this.updateMovement();
+  }
 }
